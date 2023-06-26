6413f69cadc37645f9842bfaca5836997b17ccb6
FrancoRechou <francorechou@gmail.com>
2023-06-26 00:35:17 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  const mensaje = 'Hola mundo';
+
+  return (
+    <div className="App">
+      { mensaje + ' esta es una prueba'}
+    </div>
+  );
+}
+
+export default App;
